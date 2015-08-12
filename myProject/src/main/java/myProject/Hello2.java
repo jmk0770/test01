28c942cea739930295ab1da39532ffe3b196b814
jmk0770 <jmk0770@naver.com>
2015-08-12 18:18:10 +0900

    dsafdsfds

--- myProject/src/main/java/myProject/Hello2.java
+++ myProject/src/main/java/myProject/Hello2.java
@@ -4,6 +4,10 @@ public class Hello2 {
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    int i=10;
+    while( i++ < 50)
+    System.out.println(" 뿌ㅖ루례ㅖㅜ례우ㅏㄹ무니아뤼ㅏㅇ누리ㅏ뚜ㅏ물니ㅏ");
+    
 
   }
 
